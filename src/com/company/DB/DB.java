ef3af7d56995bb3f7a060d5343c659956a8b8858
batyr23 <shutenovbatyrkhan@gmail.com>
2021-02-18 14:28:46 +0600

    blablabal

---
+++ src/com/company/DB/DB.java
@@ -0,0 +1,26 @@
+package com.company.DB;
+
+import com.company.DB.interfaces.IDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB implements IDB {
+    @Override
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+        String connectionUrl = "jdbc:postgresql://localhost:5432/e-HealthCare";
+        try {
+            // Here we load the driver’s class file into memory at the runtime
+            Class.forName("org.postgresql.Driver");
+
+            // Establish the connection
+            Connection con = DriverManager.getConnection(connectionUrl, "postgres", "sultan");
+
+            return con;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+}
