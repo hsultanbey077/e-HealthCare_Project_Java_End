d433875b4a84c74aa0e5c10d133c8e2c8d3c96ac
Batyr23 <shutenovbatyrkhan@gmail.com>
2021-02-26 11:09:48 +0600

    Sh_b

--- src/com/company/DB/DB.java
+++ src/com/company/DB/DB.java
@@ -6,6 +6,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * DB connection on PostgreSQL with connection URL
+ */
+
 public class DB implements IDB {
     @Override
     public Connection getConnection() throws SQLException, ClassNotFoundException {
@@ -14,10 +18,11 @@ public class DB implements IDB {
             // Here we load the driver’s class file into memory at the runtime
             Class.forName("org.postgresql.Driver");
 
-            // Establish the connection
+            /*Establish the connection*/
             Connection con = DriverManager.getConnection(connectionUrl, "postgres", "sultan");
 
             return con;
+
         } catch (Exception e) {
             System.out.println(e);
             return null;
--- src/com/company/DB/interfaces/IDB.java
+++ src/com/company/DB/interfaces/IDB.java
@@ -3,6 +3,10 @@ package com.company.DB.interfaces;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/**
+ * Interface for the class DB with getConnection method
+ */
+
 public interface IDB {
     Connection getConnection() throws SQLException, ClassNotFoundException;
 }
